bc088aa5bb73d347d8d868267f9eb053452e0fef
askerovk <askerovk@gmail.com>
2018-03-05 13:06:38 +0800

    Add files via upload

--- Data Science Specialization Course/10. Capstone Project/ui.R
+++ Data Science Specialization Course/10. Capstone Project/ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 shinyUI(fluidPage(
-    titlePanel(h1("Four-gram word prediction model")),
+    titlePanel(h1("Five-gram word prediction model")),
     sidebarLayout(
         sidebarPanel(h3("Instructions"), p("1. Please wait for the model to load for the first time."), p("2. Once the first 10 suggested words appear, you may begin by either typing some text or selecting one of the suggested starting words."), p("3. The model will update the choice of word predictions based on your input."), width = 4),
         mainPanel(
